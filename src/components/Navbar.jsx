9299ecb96ea2697a85a4cf50b9460befdb162419
N Johnson <johnsonnabaluru@gmail.com>
2024-07-17 10:20:17 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from "../assets/JohnsonLogo.png"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className=' flex items-center justify-between  py-6'>
+        <div className='flex flex-shrink-0 items-center' >
+          <img src={logo} alt="logo" className='mx-2 w-12  rounded-md	border-radius: 0.375rem; /* 6px */'  />
+          </div>
+          <div className='m-8 flex items-center justify-center gap-4 text-2xl ' >
+            <a href="https://www.linkedin.com/in/johnsonnabaluru/"><FaLinkedin/></a>
+            <a href="https://github.com/Johnsonnabaluru"><FaGithub/></a>
+            <a href="https://www.instagram.com/_johns0n._/"><FaInstagram/></a>
+            <a href="https://x.com/nabalurujohnson"><FaSquareXTwitter/></a>
+          </div>
+       
+    </nav>
+  )
+}
+
+export default Navbar
+ 
---
+++ src/constants/index.js
@@ -0,0 +1,29 @@
+import project1 from "../assets/projects/project-1.jpeg";
+import project2 from "../assets/projects/project-2.jpeg";
+
+
+export const HERO_CONTENT = `I am a passionate software Engineer with a knack for crafting robust and scalable web applications.I have honed my skills in front-end technologies like HTML, CSS, Javascript, React , as well as back-end technologies like Node.js, Python, SQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am actively seeking opportunities to collaborate with experienced professionals. Eager to learn and apply emerging technologies and best practices, my goal is to deliver an exceptional user experience.`
+export const PROJECTS = [
+  {
+    title: "Expense Tracker",
+    image: project1,
+    description:
+      "Expense Tracker enables users to monitor their expenses efficiently.",
+    technologies: ["HTML5", "CSS3", "React"],
+  },
+  {
+    title: "Employee Manager Pro",
+    image: project2,
+    description:
+      "Employee Manager Pro is a robust CRUD web app for seamless employee data management.",
+    technologies: ["React", "Tailwind.css ", "Node.js", "Express.js","MongoDB"],
+  },
+];
+
+export const CONTACT = {
+  address: "18 btm 2nd stage, Bengaluru , pin:560076, India",
+  phoneNo: "+91 8688297044 ",
+  email: "johnsonnabaluru@gmail.com",
+};
